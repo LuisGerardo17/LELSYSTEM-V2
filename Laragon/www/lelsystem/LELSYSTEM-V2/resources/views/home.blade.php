2ba1f57882bf52c688eb84d87c3c99eef5cf5e77
LuisGerardo17 <you@example.com>
2022-05-11 11:48:20 -0500

    login

---
+++ Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('title', 'Home')
+@section('content')
+<h1>  Bienbenido  a la aplicacion</h1>
+@endsection
---
+++ Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')- LELSYSTEM-V2</title>
+
+    <!-- Bootstrap 4.1.1 -->
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
+    <!-- Ionicons -->
+    <link href="//fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link href="{{ asset('assets/css/@fortawesome/fontawesome-free/css/all.css') }}" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/iziToast.min.css') }}">
+    <link href="{{ asset('assets/css/sweetalert.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('assets/css/select2.min.css') }}" rel="stylesheet" type="text/css"/>
+
+</head>
+<body>
+
+    @yield('content')
+    
+</body>
+</html>
