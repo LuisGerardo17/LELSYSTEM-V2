2d507477040512d5c598847623aa95cb8770679c
LuisGerardo17 <you@example.com>
2022-06-07 12:03:57 -0500

    roles

--- Laragon/www/lelsystem/LELSYSTEM-V2/database/seeders/DatabaseSeeder.php
+++ Laragon/www/lelsystem/LELSYSTEM-V2/database/seeders/DatabaseSeeder.php
@@ -15,13 +15,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $user = new User;
-         $user->name = 'Admin';
-         $user->email = 'gerardo17@gmail.com';
-         $user->password = 'potosi1988';
-
-         
-         $user->role = 'admin';
+          $user = new User;
+          $user->name = 'Admin';
+          $user->email = 'gerardo17@gmail.com';
+          $user->password = 'potosi1988';
+          $user->role = 'admin';
 
          $user ->save();
         
