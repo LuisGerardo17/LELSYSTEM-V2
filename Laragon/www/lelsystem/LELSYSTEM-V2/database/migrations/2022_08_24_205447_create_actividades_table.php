5022c606d8292b538002f3f27e93cd3036dd9169
¨Leonardo <98435590+LeonardoGBM@users.noreply.github.com>
2022-08-24 16:37:49 -0500

    migracion

---
+++ Laragon/www/lelsystem/LELSYSTEM-V2/database/migrations/2022_08_24_205447_create_actividades_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActividadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actividades', function (Blueprint $table) {
+            $table->id('codigo_actividad')->unique();
+            $table->string('nombre_actividad');
+            $table->string('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actividades');
+    }
+}
