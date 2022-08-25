d542d156f3337896f692d1c3700c3b1945fdadec
Enrique Mera <esy.mera@yavirac.edu.ec>
2022-08-25 11:08:45 -0500

    hola

---
+++ Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/admin/layouts/adminCabeza.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Admin</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="{{ asset('admin/css/main.css') }}">
+</head>
+<body>
---
+++ Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/admin/layouts/adminpies.blade.php
@@ -0,0 +1,13 @@
+<!--====== Scripts -->
+<script src="{{ asset('admin/js/jquery-3.1.1.min.js') }}"></script>
+	<script src="{{ asset('admin/js/sweetalert2.min.js') }}"></script>
+	<script src="{{ asset('admin/js/bootstrap.min.js') }}"></script>
+	<script src="{{ asset('admin/js/material.min.js') }}"></script>
+	<script src="{{ asset('admin/js/ripples.min.js') }}"></script>
+	<script src="{{ asset('admin/js/jquery.mCustomScrollbar.concat.min.js') }}"></script>
+	<script src="{{ asset('admin/js/main.js') }}"></script>
+	<script>
+		$.material.init();
+	</script>
+</body>
+</html>
