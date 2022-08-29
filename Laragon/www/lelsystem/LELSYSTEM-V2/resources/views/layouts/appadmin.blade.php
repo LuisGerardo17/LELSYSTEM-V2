3ed08fe15334fd62225ec4380349e823e41b96f8
Enrique Mera <esy.mera@yavirac.edu.ec>
2022-08-29 12:56:36 -0500

    Revert "Merge branch 'f-b-lelsystemv2' into enriqueRama"
    This reverts commit 2220ae9a1adf7def27777668e70dac06fb252317, reversing
    changes made to c4412eae553d105184116bc7163187cdfb9a44c5.

---
+++ Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/layouts/appadmin.blade.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>LELSYSTEM</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">  
+    <link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('static/css/admin.css') }}"/>
+
+</head>
+
+<body>
+
+    <header class="cabecera">
+        <div class="container-xl logo-container">
+            <a href="#" class="logo"><b>PLATAFORMA @LELSYSTEM MODULO ADMINISTRADOR</b></a>
+            <nav class="lista">
+                <ul>
+                    @if(auth()->check())
+                      <li class ="mx-8">
+                        <p class="text-xl">Welcome <b>{{auth()->user()->name}}</b></p>
+                      </li>
+                      <li class="naa"><a href="{{route('login.destroy')}}"><h5><b>Log Out</b> </h5> </a>
+                        
+                      </li>
+                      <i class="cerrar icon ion-md-person"></i>
+                
+                    @else
+                       <li class="nav-item sesiones">
+                        <a class="nav-link marign colorq" href="{{route('login')}}">Login</a>
+                      </li>
+                      <li class="nav-item sesiones">
+                        <a class="nav-link marign colorq" href="{{route('register')}}">Register</a>
+                      </li>
+                   @endif
+             </ul>
+                
+            </nav>
+        </div>
+    </header>
+
+
+    <main>
+        <center>
+            <div class="container containera pt-1">
+                <hr>
+                <div class="row text-center py-1">
+                    <h1><b>BIENVENIDO</b> </h1>
+                </div>
+                <hr>
+                <div class="row py-2">
+                    <h3><b> QUE HARAS HOY</b></h3>
+                </div>
+                <div class="row py-3">
+                    <div class="col">
+                        <a href="Gestionar usuarios.html">
+                            <div class="card carda" style="width: 18rem;">
+                                <img src="static/img/usuarios.png" class="card-img-top" height="250px" alt="...">
+                                <hr>
+                                <div class="card-body">
+                                    <h5 class="btn btn-dark">GESTIONAR USUARIOS</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="curso.html">
+                            <div class="card carda" style="width: 18rem;">
+                                <img src="static/img/curso-virtual-removebg-preview.png" height="250px" class="card-img-top" alt="...">
+                                <hr>
+                                <div class="card-body">
+                                    <h5 class="btn btn-dark">GESTIONAR CURSOS</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="row py-3">
+                    <div class="col">
+                        <a href="items.html">
+                            <div class="card carda" style="width: 18rem;">
+                                <img src="static/img/internet-removebg-preview.png" height="250px" class="card-img-top" alt="...">
+                                <hr>
+                                <div class="card-body">
+                                    <h5 class="btn btn-dark">GESTIONAR ACTIVIDADES</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="recursos.html">
+                            <div class="card carda" style="width: 18rem;">
+                                <img src="static/img/recursos-removebg-preview.png" height="250px" class="card-img-top" alt="...">
+                                <hr>
+                                <div class="card-body">
+                                    <h5 class="btn btn-dark">GESTIONAR RECURSOS</h5>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <br><br>
+            </div>
+        </center>
+    </main>
+   
+
+
+   
+    <section class="">
+
+        <footer class="text-center text-white" style="background-color: rgb(62, 65, 68)">
+
+            <div class="container p-4 pb-0">
+
+                <section class="">
+                    <p class="d-flex justify-content-center align-items-center">
+                        <span class="me-3">Pagina Oficial &nbsp;</span>
+                        <button type="button" class="btn btn-outline-light btn-rounded">
+                            https://lelsYstem@.edu.ec
+                        </button>
+                    </p>
+                </section>
+            </div>
+
+
+            <div class="text-center p-3" style="background-color: rgb(5, 5, 5);">
+                © 2022 LELSYSTEM:
+                <a class="text-white" href="https://mdbootstrap.com/">LELSYSTEM.com</a>
+            </div>
+        </footer>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
