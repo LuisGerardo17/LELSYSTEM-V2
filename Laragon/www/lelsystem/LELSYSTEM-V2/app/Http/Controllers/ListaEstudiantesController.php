2fa9778084bdbe8bde6e37befc17a0073d6b1254
Enrique Mera <esy.mera@yavirac.edu.ec>
2022-09-08 14:41:10 -0500

    aaaa

---
+++ Laragon/www/lelsystem/LELSYSTEM-V2/app/Http/Controllers/ListaEstudiantesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ListadoEstudiantes;
+use Illuminate\Http\Request;
+
+class ListaEstudiantesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ListadoEstudiantes  $listadoEstudiantes
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ListadoEstudiantes $listadoEstudiantes)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ListadoEstudiantes  $listadoEstudiantes
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ListadoEstudiantes $listadoEstudiantes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ListadoEstudiantes  $listadoEstudiantes
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ListadoEstudiantes $listadoEstudiantes)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ListadoEstudiantes  $listadoEstudiantes
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ListadoEstudiantes $listadoEstudiantes)
+    {
+        //
+    }
+}
