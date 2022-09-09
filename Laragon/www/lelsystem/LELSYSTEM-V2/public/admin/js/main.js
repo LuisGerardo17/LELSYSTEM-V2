4248e4e74138ee8495227e789e0ec618a7b17045
Enrique Mera <esy.mera@yavirac.edu.ec>
2022-09-09 15:55:51 -0500

    ff

--- Laragon/www/lelsystem/LELSYSTEM-V2/public/admin/js/main.js
+++ Laragon/www/lelsystem/LELSYSTEM-V2/public/admin/js/main.js
@@ -109,13 +109,13 @@ btnAbrirPopup.forEach(e => {
 	let overlay = document.getElementById("overlay"),
     popup = document.getElementById("popup"),
     btnCerrarPopup = document.getElementById("btn-cerrar-popup");
-	
+
 	e.addEventListener("click", function(){
 	overlay.classList.add("active");
 	popup.classList.add("active");
 	});
-		
-	e.addEventListener("click", function(){
+
+	btnCerrarPopup.addEventListener("click", function(){
 	overlay.classList.remove("active");
 	popup.classList.remove("active");
 	});
