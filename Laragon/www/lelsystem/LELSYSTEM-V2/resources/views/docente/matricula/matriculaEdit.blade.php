2a9e138acb51322a7369e6a13674c7b8fe39c07a
¨Leonardo <98435590+LeonardoGBM@users.noreply.github.com>
2022-09-12 11:04:24 -0500

    b

--- Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/docente/matricula/matriculaEdit.blade.php
+++ Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/docente/matricula/matriculaEdit.blade.php
@@ -12,7 +12,7 @@
 	<div class="row">
 		<div class="col-xs-12">
 			<ul class="nav nav-tabs" style="margin-bottom: 15px;">
-			<li><a href="#list" data-toggle="tab">Lista</a></li>
+				<li><a href="#list" data-toggle="tab">Lista</a></li>
 			</ul>
 			<div id="myTabContent" class="tab-content">
 				<div class="tab-pane fade active in" id="new">
@@ -20,60 +20,59 @@
 						<div class="row">
 							<div class="col-xs-12 col-md-10 col-md-offset-1">
 								<form action="{{ url('matricula/'.$matricula->cedula) }}" method="POST">
-								@csrf
-								<div class="form-group label-floating">
-                                    <label class="control-label">Cedula</label>
-                                    <input class="form-control" type="text" name="cedula" value="{{ isset($matricula) ? $matricula->cedula : old('cedula') }}" {{ isset($matri) ? 'disabled' : '' }}">
-                                </div>
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Nombres</label>
-                                    <input class="form-control" type="text" name="nombres" value="{{ isset($matricula) ? $matricula->nombres : old('nombres') }}" {{ isset($matri) ? 'disabled' : '' }}">
-                                </div>
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Apellidos</label>
-                                    <input class="form-control" type="text" name="apellidos" value="{{ isset($matricula) ? $matricula->apellidos : old('apellidos') }}" {{ isset($matri) ? 'disabled' : '' }}">
-                                </div>
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Correo</label>
-                                    <input class="form-control" type="text" name="correo" value="{{ isset($matricula) ? $matricula->correo : old('correo') }}" {{ isset($matri) ? 'disabled' : '' }}">
-                                </div>
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Codigo curso</label>
-                                    <input class="form-control" type="text" name="codigo_curso" value="{{$matricula->codigo_curso}}">
-                                </div>
-                                <div class="form-group label-floating">
-                                    <label class="control-label">Estado</label>
-                                    <br>
-                                    <input name="estado" type="radio" value="{{$matricula->estado}}">
-                                    <label>Activa</label>
-                                    <input name="estado" type="radio" value="{{$matricula->estado}}">
-                                    <label>Inactiva</label>
-                                </div>
-                                <p class="text-center">
-                                    <button href="#!" class="btn btn-info btn-raised btn-sm"><i class="zmdi zmdi-floppy"></i> Guardar</button>
-                                </p>
+									@csrf
+									<div class="form-group label-floating">
+										<label class="control-label">Cedula</label>
+										<input class="form-control" type="text" name="cedula" value="{{ isset($matricula) ? $matricula->cedula : old('cedula') }}" {{ isset($matri) ? 'disabled' : '' }}">
+									</div>
+									<div class="form-group label-floating">
+										<label class="control-label">Nombres</label>
+										<input class="form-control" type="text" name="nombres" value="{{ isset($matricula) ? $matricula->nombres : old('nombres') }}" {{ isset($matri) ? 'disabled' : '' }}">
+									</div>
+									<div class="form-group label-floating">
+										<label class="control-label">Apellidos</label>
+										<input class="form-control" type="text" name="apellidos" value="{{ isset($matricula) ? $matricula->apellidos : old('apellidos') }}" {{ isset($matri) ? 'disabled' : '' }}">
+									</div>
+									<div class="form-group label-floating">
+										<label class="control-label">Correo</label>
+										<input class="form-control" type="text" name="correo" value="{{ isset($matricula) ? $matricula->correo : old('correo') }}" {{ isset($matri) ? 'disabled' : '' }}">
+									</div>
+									<div class="form-group label-floating">
+										<label class="control-label">Codigo curso</label>
+										<input class="form-control" type="text" name="nombre_curso" value="{{$matricula->nombre_curso}}">
+									</div>
+									<div class="form-group label-floating">
+										<label class="control-label"> Estado </label>
+										<select class="form-select" name="estado" value="{{$matricula->estado}}">
+											<option value="Activo">Activo</option>
+											<option value="Inactivo">Inactivo</option>
+										</select>
+									</div>
+									<p class="text-center">
+										<button href="#!" class="btn btn-info btn-raised btn-sm"><i class="zmdi zmdi-floppy"></i> Guardar</button>
+									</p>
 								</form>
 							</div>
 						</div>
 					</div>
 				</div>
 
-			</tbody>
-		</table>
-						<ul class="pagination pagination-sm">
-							<li class="disabled"><a href="#!">«</a></li>
-							<li class="active"><a href="#!">1</a></li>
-							<li><a href="#!">2</a></li>
-							<li><a href="#!">3</a></li>
-							<li><a href="#!">4</a></li>
-							<li><a href="#!">5</a></li>
-							<li><a href="#!">»</a></li>
-						</ul>
-					</div>
-				</div>
+				</tbody>
+				</table>
+				<ul class="pagination pagination-sm">
+					<li class="disabled"><a href="#!">«</a></li>
+					<li class="active"><a href="#!">1</a></li>
+					<li><a href="#!">2</a></li>
+					<li><a href="#!">3</a></li>
+					<li><a href="#!">4</a></li>
+					<li><a href="#!">5</a></li>
+					<li><a href="#!">»</a></li>
+				</ul>
 			</div>
 		</div>
 	</div>
+</div>
+</div>
 </div>
 </section>
 
