1a478532c3ee0bc754e1246958db2d463f078ade
Enrique Mera <esy.mera@yavirac.edu.ec>
2022-09-16 16:38:24 -0500

    ggg

--- Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/estudiante/layouts/estudiante.blade.php
+++ Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/estudiante/layouts/estudiante.blade.php
@@ -20,23 +20,107 @@
 <body>
 
 
-	<section class="full-box dashboard-contentPage" style="background-color: rgb(173, 173, 173)" >
-		<!--fondo arreglar-->
-		<!-- NavBar -->
-		<nav class="full-box dashboard-Navbar ">
-			<ul class="full-box list-unstyled text-right">
-				<li>
-					<a href="#!" class="btn-search">
-						<i class="zmdi zmdi-search"></i>
-					</a>
-				</li>
-				<li>
-					<a href="#!" class="btn-modal-help">
-						<i class="zmdi zmdi-help-outline"></i>
-					</a>
-				</li>
-			</ul>
-		</nav>
+	<style>
+        .dashboard-contentPage{
+            right: 0;
+            z-index: 0;
+            padding-left: 0px;
+        }
+        .tile{
+            width: 300px;
+            height: 450px;
+            
+        }
+        .tile-icon{
+            bottom: 0;
+            height: 400px;
+            width: 300px;
+        }
+        h4 {
+            color: #000;
+            text-align: center;
+            padding-bottom: 10px;
+        }
+        p{
+            color: #000;
+        }
+        .sup {
+            padding-bottom: 20px; 
+        }
+        img {
+            padding: 5px;
+        }
+        .dashboard-Navbar{
+            background-color: #009688; 
+            overflow: hidden;
+        }
+        
+        /*menu*/
+        .Navbar-superior{
+            width: 100%;
+            background: #009688;
+            padding: 1em;
+        }
+        .logoEmpresa{
+            background: #fff;
+            border-radius: 10%;
+        }
+        .avatar{
+            float: right;
+        }
+        .a{
+            color: #fff;
+            
+        }
+        button{
+            background: transparent;
+            color: #fff;
+            border: none;
+            font-size: 22px;
+            float: right;
+            padding-bottom: 10px;
+        }
+        .nav{
+            color: #fff;
+            float: right;
+            display: none;
+            position: relative;
+            top: 30px;
+            left: 10%;
+        }
+        .activo{
+            display: block;
+        }
+        .ul{
+            background: rgba(0,0,0,.6);
+        }
+        .li{
+            margin: .5em 0;
+            
+        }
+        
+        </style>
+        
+            <!-- menu-->
+            <section class="full-box dashboard-contentPage" style="background-color: rgb(173, 173, 173)">
+                    
+                <div class="Navbar-superior">
+                    <img class="logoEmpresa" width="150px" src="{{ asset('static/img/logo5.png') }}" alt="">
+                    <img  class="avatar" width="100px" src="{{ asset('admin/assets/img/avatar.jpg') }}" alt="">
+                    <button id="button">
+                        NOMBRE ESTUDIANTE<i class="zmdi-hc-fw zmdi zmdi-caret-down"></i>
+                    </button>
+                    <nav class="nav" id="nav">
+                        <ul class="ul" >
+                            <li class="li">
+                                <a class="a" href="#">Area personal</a>
+                            </li>
+                            <li class="li">
+                                <a class="a" href="#">Cerrar seción</a>
+                            </li>
+                        </ul>
+                    </nav>	
+                </div>
 
         @yield('seccion')
 
