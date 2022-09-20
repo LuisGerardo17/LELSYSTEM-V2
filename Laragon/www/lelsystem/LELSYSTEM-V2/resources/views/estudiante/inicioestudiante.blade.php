1129d20d2978c98182c27c74ac83d6dfec79d353
centii123 <102315503+centii123@users.noreply.github.com>
2022-09-20 09:00:06 -0500

    error

--- Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/estudiante/inicioestudiante.blade.php
+++ Laragon/www/lelsystem/LELSYSTEM-V2/resources/views/estudiante/inicioestudiante.blade.php
@@ -1,115 +1,8 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<title>A</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link rel="stylesheet" href="{{ asset('admin/css/main.css') }}">
-</head>
+@extends('estudiante.layouts.estudiante')
+@section('fondo','rgb(233, 233, 233)')
+@section('titulo', 'Index')
+@section('seccion')
 
-<style>
-.dashboard-contentPage{
-	right: 0;
-	z-index: 0;
-	padding-left: 0px;
-}
-.tile{
-    width: 300px;
-	height: 450px;
-    
-}
-.tile-icon{
-	bottom: 0;
-	height: 400px;
-	width: 300px;
-}
-h4 {
-    color: #000;
-    text-align: center;
-    padding-bottom: 10px;
-}
-p{
-    color: #000;
-}
-.sup {
-    padding-bottom: 20px; 
-}
-img {
-    padding: 5px;
-}
-.dashboard-Navbar{
-	background-color: #009688; 
-	overflow: hidden;
-}
-
-/*menu*/
-.Navbar-superior{
-	width: 100%;
-	background: #009688;
-	padding: 1em;
-}
-.logoEmpresa{
-	background: #fff;
-	border-radius: 10%;
-}
-.avatar{
-	float: right;
-}
-.a{
-	color: #fff;
-	
-}
-button{
-	background: transparent;
-	color: #fff;
-	border: none;
-	font-size: 22px;
-	float: right;
-	padding-bottom: 10px;
-}
-.nav{
-	color: #fff;
-	float: right;
-	display: none;
-	position: relative;
-    top: 30px;
-	left: 10%;
-}
-.activo{
-	display: block;
-}
-.ul{
-	background: rgba(0,0,0,.6);
-}
-.li{
-	margin: .5em 0;
-}
-
-</style>
-<body>
-	
-
-	<!-- menu-->
-	<section class="full-box dashboard-contentPage">
-	        
-		<div class="Navbar-superior">
-		    <img class="logoEmpresa" width="150px" src="{{ asset('static/img/logo5.png') }}" alt="">
-			<img  class="avatar" width="100px" src="{{ asset('admin/assets/img/avatar.jpg') }}" alt="">
-			<button id="button">
-			    NOMBRE ESTUDIANTE<i class="zmdi-hc-fw zmdi zmdi-caret-down"></i>
-			</button>
-			<nav class="nav" id="nav">
-			    <ul class="ul" >
-				    <li class="li">
-						<a class="a" href="#">Area personal</a>
-					</li>
-				    <li class="li">
-						<a class="a" href="#">Cerrar seción</a>
-					</li>
-			    </ul>
-			</nav>	
-		</div>
-		
 		<!-- Content page -->
 		<div class="container-fluid">
 			<div class="page-header text-center">
@@ -124,7 +17,7 @@ button{
 					Arte Culinario
 				</div>
 				<div class="full-box tile-icon text-center">
-                    <h4>Ing. Anshelo Proaño</h4> 
+                    <h4>Ing. Anshelo Proaño</h4>
                     <img class="sup" width="90%" src="{{ asset('admin/assets/img/img.jpg') }}" alt="arte">
                     <img width="12%" src="{{ asset('admin/assets/img/what.png') }}"><a href="#" class="ic">+593 999999999</a> <br><br>
                     <img width="12%" src="{{ asset('admin/assets/img/gmail.png') }}"><a href="#" class="ic">aal.proano@yavirac.edu.ec</a>
@@ -137,13 +30,13 @@ button{
 					control de incendios
 				</div>
 				<div class="full-box tile-icon text-center">
-                    <h4>Ing. Anshelo Proaño</h4> 
+                    <h4>Ing. Anshelo Proaño</h4>
                     <img class="sup" width="90%" src="{{ asset('admin/assets/img/img.jpg') }}" alt="arte">
                     <img width="12%" src="{{ asset('admin/assets/img/what.png') }}"><a href="#" class="ic">+593 999999999</a> <br><br>
                     <img width="12%" src="{{ asset('admin/assets/img/gmail.png') }}"><a href="#" class="ic">aal.proano@yavirac.edu.ec</a>
 				</div>
 			</article>
-			
+
 		</a>
 		<a href="#">
 			<article class="full-box tile">
@@ -151,7 +44,7 @@ button{
 					desarrollo de software
 				</div>
 				<div class="full-box tile-icon text-center">
-                    <h4>Ing. Anshelo Proaño</h4> 
+                    <h4>Ing. Anshelo Proaño</h4>
                     <img class="sup" width="90%" src="{{ asset('admin/assets/img/img.jpg') }}" alt="arte">
                     <img width="12%" src="{{ asset('admin/assets/img/what.png') }}"><a href="#" class="ic">+593 999999999</a> <br><br>
                     <img width="12%" src="{{ asset('admin/assets/img/gmail.png') }}"><a href="#" class="ic">aal.proano@yavirac.edu.ec</a>
@@ -159,21 +52,12 @@ button{
 			</article>
 		</a>
 		</div>
-		
-	<!-- NavBar -->
+
+	<!-- NavBar
 	<nav class="full-box dashboard-Navbar">
 			<div class="full-box text-center text-uppercase">
                 <h3 class="titulo">BIENVENIDO ESTUDIANTE</h3>
 			</div>
-		</nav>
+		</nav>-->
+@endsection
 
-		<script>
-	        const button = document.querySelector('#button')
-	        const nav    = document.querySelector('#nav')
-
-	        button.addEventListener('click', ()=>{
-		    nav.classList.toggle('activo')
-	        })
-        </script>
-    </body>
-    
