753bbb0117f7f3462cc17c83afcdbab2df29c746
centii123 <102315503+centii123@users.noreply.github.com>
2022-10-03 10:44:57 -0500

    k

--- Laragon/www/lelsystem/LELSYSTEM-V2/app/Http/Controllers/LoginController.php
+++ Laragon/www/lelsystem/LELSYSTEM-V2/app/Http/Controllers/LoginController.php
@@ -6,7 +6,6 @@ use App\Models\Estudiantes;
 use App\Models\Docentes;
 use App\Models\Administradores;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 
 class LoginController extends Controller
 {
